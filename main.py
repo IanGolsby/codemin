63820fbfd65e556d5d25d41d2dd2e8c0559ebb68
Ian Golsby <iangolsby49@gmail.com>
2018-06-21 10:30:31 -0500

    Add var and function check

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ f = open('premin', 'r')
 
 f_text = f.read()
 
-f_text = f_text.replace(' ','')
 f_text = f_text.replace('\n','')
 f_text = f_text.replace('	','')
+
+for c in f_text:
+	if c == ' ':
+		if f_text[f_text.index(c)-4:f_text.index(c) != 'var ' and f_text[f_text.index(c)-9:f_text.index(c) != 'function ':
+				  f_text = f_text.replace(' ','')
